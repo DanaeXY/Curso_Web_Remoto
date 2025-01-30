55d8e1c313c35363c37340e3ff74ee6f71cc96ff
Vanessa <vanebeis80@gmail.com>
2025-01-30 18:07:51 +0100

    "probando 100%"

---
+++ Modulo_2/UD01_2/static/masElementos.js
@@ -0,0 +1,7 @@
+let app = document.getElementById("app");
+
+let elementoDiv = document.createElement("div");
+
+elementoDiv.innerHTML = "Todos los días sale el sol, chipirón!";
+
+app.appendChild(elementoDiv);
