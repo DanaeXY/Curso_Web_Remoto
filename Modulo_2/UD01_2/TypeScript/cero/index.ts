a8037c050cb27082eed92f921e30e4bee88eaca7
Vanessa <vanebeis80@gmail.com>
2025-02-13 19:53:16 +0100

    clases en typescript

---
+++ Modulo_2/UD01_2/TypeScript/cero/index.ts
@@ -0,0 +1,14 @@
+class Numero {
+    constructor(){
+        
+    }
+    
+    private mostrarNumero () :void{
+        let numero: number = 10;
+
+        console.log("El número es:", numero);
+    }
+}
+/* let numero: number = 10;
+
+console.log("El número es:", numero); */
