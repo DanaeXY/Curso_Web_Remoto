8f51541ecf74fa74095c16096f5fbeaec661d576
Vanessa <vanebeis80@gmail.com>
2025-02-18 19:41:19 +0100

    creando carpeta para ejercicios typescript

--- Typescript-ONLY/public/javascript/Clases/Etiqueta.js
+++ Typescript-ONLY/public/javascript/Clases/Etiqueta.js
@@ -1,3 +1,9 @@
-class Etiqueta {
-}
-export { Etiqueta };
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Etiqueta = void 0;
+var Etiqueta = /** @class */ (function () {
+    function Etiqueta() {
+    }
+    return Etiqueta;
+}());
+exports.Etiqueta = Etiqueta;
